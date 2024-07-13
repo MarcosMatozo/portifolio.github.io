1fed717863c39fe20a04fea8cdb39ccfa8704f3e
MarcosMatozo <marcoshenriquesp@hotmail.com>
2024-07-13 19:14:31 -0300

    Updates

---
+++ precache-manifest.563a926efa09426930678581f65e1489.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f389980d5a046caf90213fa0a95254e",
+    "url": "/marcos-matozo-portifolio/index.html"
+  },
+  {
+    "revision": "af6c42823e8094f985c5",
+    "url": "/marcos-matozo-portifolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0f4ab2ac3a3447c38fc5",
+    "url": "/marcos-matozo-portifolio/static/js/2.ce14387d.chunk.js"
+  },
+  {
+    "revision": "7373c6e4eb6af1a9d439",
+    "url": "/marcos-matozo-portifolio/static/js/3.ba8b7f13.chunk.js"
+  },
+  {
+    "revision": "af6c42823e8094f985c5",
+    "url": "/marcos-matozo-portifolio/static/js/main.04dc5252.chunk.js"
+  },
+  {
+    "revision": "c36f0cd0735ce44df30b",
+    "url": "/marcos-matozo-portifolio/static/js/runtime~main.37e26b8d.js"
+  }
+]);
